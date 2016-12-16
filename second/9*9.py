889e6e0598d34a616792ee42460c8cd989244da7
RaoPeng <raopeng@bgp-bj-211-152-009-188.cdn.fastweb.com.cn>
2016-12-16 15:37:08 +0800

    third commit

---
+++ second/9*9.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+#   author:RaoPeng
+for i in range(1,10):
+	temp = ''
+	for j in range(1,10):
+		if i>=j:
+			temp += '%s * %s = %s  ' %(i,j,i*j)
+	print temp
---
+++ second/bubble_sort.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+#   author:RaoPeng
+arr = [3,7,18,2,20,10000,99,1,54,100,4,50,120,200,450]
+count = 0
+for i in range(len(arr)):
+	for j in range(len(arr)-1-i):
+		if arr[j]>arr[j+1]:
+			arr[j],arr[j+1] = arr[j+1],arr[j]
+		print arr
+		print '='*50
+		count += 1
+print count
+print arr
