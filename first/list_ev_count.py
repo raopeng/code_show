8aeb33b59fd858cf51ff407bcc435a3f61e93d21
RaoPeng <raopeng@bgp-bj-211-152-009-188.cdn.fastweb.com.cn>
2016-12-16 10:33:59 +0800

    second commit

---
+++ first/list_ev_count.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+#   author:RaoPeng
+arr = ['C','js','python','js','css','js','html','node','js','python','js','css','js','html','node','js','python','js','css','js','html','node','css','js','html','node','js','python','js','css','js','html','node','js','python','js','css','js','html','node']
+dic = {}
+for i in arr:
+	if i in dic:
+		dic[i] += 1
+	else:
+		dic[i] = 1
+print dic
